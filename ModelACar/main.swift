6b3080f690614facf8197b02d1b1c50274b7c87a
Matthew Riley <matthewriley@interapts-macbook-pro-10.local>
2018-08-20 14:30:55 -0500

    Initial Commit

---
+++ ModelACar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ModelACar
+//
+//  Created by Matthew Riley on 8/20/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
